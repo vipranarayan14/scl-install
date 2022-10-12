bc01e65944e3e7ffb796ad4560b3412b4b963fe1
Prasanna Venkatesh T S <17687018+vipranarayan14@users.noreply.github.com>
2022-10-12 15:25:25 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # Installation location
 
 MAIN_DIR="$HOME/scl"
-ZEN_MAIN_DIR="$MAIN_DIR/SKT"
+ZEN_MAIN_DIR="$MAIN_DIR/SKT/zen"
 SCL_MAIN_DIR="$MAIN_DIR/scl"
 
 
@@ -47,7 +47,7 @@ echo -e "\n=== Downloading SCL & Dependencies ===\n"
 mkdir $MAIN_DIR
 
 # Download Zen
-git clone --depth 1 https://gitlab.inria.fr/huet/Zen.git "$ZEN_MAIN_DIR/zen"
+git clone --depth 1 https://gitlab.inria.fr/huet/Zen.git "$ZEN_MAIN_DIR"
 
 echo -e "\n"
 
@@ -56,20 +56,20 @@ git clone --depth 1 https://github.com/samsaadhanii/scl.git "$SCL_MAIN_DIR"
 
 cat "$SCL_MAIN_DIR/SPEC/spec_users.txt" \
 | sed -E \
-    -e "s|^(ZENDIR=).*$|\1$ZEN_MAIN_DIR/zen/ML|" \
+    -e "s|^(ZENDIR=).*$|\1$ZEN_MAIN_DIR/ML|" \
     -e "s|^(SCLINSTALLDIR=).*$|\1$SCL_MAIN_DIR|" \
 > "$SCL_MAIN_DIR/spec.txt"
 
 
+# Setup Zen
 echo -e "\n=== Installing SCL ===\n"
 
-if [ ! -d "$ZEN_MAIN_DIR/zen/ML" ] || [ ! -d "$SCL_MAIN_DIR" ]; then 
+if [ ! -d "$ZEN_MAIN_DIR/ML" ] || [ ! -d "$SCL_MAIN_DIR" ]; then 
     echo "Missing dependencies. Installation failed."
     exit 1
 fi
 
-# Setup Zen
-cd "$ZEN_MAIN_DIR/zen/ML"
+cd "$ZEN_MAIN_DIR/ML"
 
 make
 
