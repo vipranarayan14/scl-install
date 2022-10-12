df86212f7ac7a0318188c2cb2788731641df0784
Prasanna Venkatesh T S <17687018+vipranarayan14@users.noreply.github.com>
2022-10-12 15:29:22 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# Installation location
-
+# Installation paths
 MAIN_DIR="$HOME/scl"
 ZEN_MAIN_DIR="$MAIN_DIR/SKT/zen"
 SCL_MAIN_DIR="$MAIN_DIR/scl"
@@ -54,21 +53,16 @@ echo -e "\n"
 # Download SCL
 git clone --depth 1 https://github.com/samsaadhanii/scl.git "$SCL_MAIN_DIR"
 
-cat "$SCL_MAIN_DIR/SPEC/spec_users.txt" \
-| sed -E \
-    -e "s|^(ZENDIR=).*$|\1$ZEN_MAIN_DIR/ML|" \
-    -e "s|^(SCLINSTALLDIR=).*$|\1$SCL_MAIN_DIR|" \
-> "$SCL_MAIN_DIR/spec.txt"
 
-
-# Setup Zen
 echo -e "\n=== Installing SCL ===\n"
+
 
 if [ ! -d "$ZEN_MAIN_DIR/ML" ] || [ ! -d "$SCL_MAIN_DIR" ]; then 
     echo "Missing dependencies. Installation failed."
     exit 1
 fi
 
+# Setup Zen
 cd "$ZEN_MAIN_DIR/ML"
 
 make
@@ -77,8 +71,13 @@ sudo a2enmod cgid
 sudo systemctl restart apache2
 
 # Install SCL
-
 cd "$SCL_MAIN_DIR"
+
+cat "$SCL_MAIN_DIR/SPEC/spec_users.txt" \
+| sed -E \
+    -e "s|^(ZENDIR=).*$|\1$ZEN_MAIN_DIR/ML|" \
+    -e "s|^(SCLINSTALLDIR=).*$|\1$SCL_MAIN_DIR|" \
+> "$SCL_MAIN_DIR/spec.txt"
 
 ./configure
 
